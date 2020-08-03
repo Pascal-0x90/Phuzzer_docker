e05dbbe95834204bd9ad1bf3b46525b6c16093be
Nathan <35417308+Pascal-0x90@users.noreply.github.com>
2020-08-03 15:30:45 -0700

    Updated to take arguments

--- src/startup.sh
+++ src/startup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+binary=$1
 # Compile binary
-gcc chall.bin.c -o chall.bin
+./setup.sh /usr/local/bin/afl-unix
 
 # Sets kernel options for AFL
 echo core > /proc/sys/kernel/core_pattern
 echo 1 > /proc/sys/kernel/sched_child_runs_first
 
 # run
-python3 -m phuzzer -t 30 -c 4 -d 2 "$(pwd)/chall.bin"
+python3 -m phuzzer -t 30 -c 4 -d 2 "$(pwd)/build/$binary"
