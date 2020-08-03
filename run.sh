5a14225237deaba82079f09e8a3c1158e9f6cf2e
Pascal <pascal-0x90@protonmail.com>
2020-08-02 21:22:43 -0700

    Added structure for running.

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 docker run -u "root" --privileged \
     -v "$(pwd)/src:/phuzzui" \
-    -it pascal0x90/basic_phuzzer \
+    -it phuzzer \
     /bin/bash -c "cd /phuzzui && ./startup.sh"
--- src/startup.sh
+++ src/startup.sh
@@ -8,4 +8,4 @@ echo core > /proc/sys/kernel/core_pattern
 echo 1 > /proc/sys/kernel/sched_child_runs_first
 
 # run
-python3 -m phuzzer -i -c 4 -d 2 "$(pwd)/chall.bin"
+python3 -m phuzzer -c 4 -d 2 "$(pwd)/chall.bin"
