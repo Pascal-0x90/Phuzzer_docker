26bea810f0c6ee701833d761b86a2301c94a8a8d
mahaloz <zbasque@asu.edu>
2020-08-03 15:22:48 -0700

    updated workdir

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 docker run -u "root" --privileged \
     -v "$(pwd)/src:/phuzzui" \
     -it pascal0x90/basic_phuzzer \
-    /bin/bash -c "cd /phuzzui && ./startup.sh"
+    /bin/bash -c "./startup.sh"
