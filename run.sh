9431bd5c53e53561f84a1574a69ebcd5b59f8bca
Nathan <35417308+Pascal-0x90@users.noreply.github.com>
2020-08-03 15:33:13 -0700

    Update to take argument

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Add name of binary
+binary=$1
+
+# Run container
 docker run -u "root" --privileged \
     -v "$(pwd)/src:/phuzzui" \
     -it pascal0x90/basic_phuzzer \
-    /bin/bash -c "./startup.sh"
+    /bin/bash -c "./startup.sh $binary"
