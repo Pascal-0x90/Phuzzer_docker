b050d479e294d463318005c97772cc3557995de1
mahaloz <zbasque@asu.edu>
2020-08-04 10:42:20 -0700

    fixed phuzzer reporting

--- src/startup.sh
+++ src/startup.sh
@@ -4,6 +4,13 @@
 rm /usr/bin/python
 ln -s /usr/bin/python3 /usr/bin/python
 
+# fix phuzzer
+cd /usr/local/lib/python3.6/dist-packages/phuzzer 
+git init
+cd ..
+git apply /phuzzui/drift.patch
+cd /phuzzui/src
+
 # Take in name of binary
 binary=$1
 
@@ -23,4 +30,4 @@ echo core > /proc/sys/kernel/core_pattern
 echo 1 > /proc/sys/kernel/sched_child_runs_first
 
 # run
-python3 -m phuzzer -t 30 -c 4 -d 2 "$(pwd)/harness/build/$binary"
+python3 -m phuzzer -c 1 -s ./seed_dir "$(pwd)/harness/build/$binary"
